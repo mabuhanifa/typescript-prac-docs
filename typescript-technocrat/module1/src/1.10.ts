7aa295475e176de9e5c7401c194277abf38bb5db
mabuhanifa <moh.abuhanifa@gmail.com>
2024-05-30 12:35:08 +0600

    Union And Intersection Types

--- typescript-technocrat/module1/src/1.10.ts
+++ typescript-technocrat/module1/src/1.10.ts
@@ -3,14 +3,34 @@
   type Gender = "Male" | "Female";
   const employee: UnionType = "Dev";
   const employee2: UnionType = "QA";
+
   type User = {
     name: string;
     age: number;
     gender: Gender;
   };
+
+  type FrontendDeveloper = {
+    skills: string[];
+    designation1: "Frontend Developer";
+  };
+
+  type BackendDeveloper = {
+    skills: string[];
+    designation2: "Backend Developer";
+  };
+
   const user: User = {
     name: "Kabir",
     age: 25,
     gender: "Male",
+  };
+
+  type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
+
+  const fullStackDeveloper: FullstackDeveloper = {
+    skills: ["HTML", "CSS", "EXPRESS"],
+    designation1: "Frontend Developer",
+    designation2: "Backend Developer",
   };
 }
